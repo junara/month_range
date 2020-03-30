0d72668f773ee96866049480831fd426919cd70b
junara <jun5araki@gmail.com>
2020-03-30 09:42:06 +0900

    require_relative

--- lib/month_range.rb
+++ lib/month_range.rb
@@ -4,5 +4,7 @@ require_relative 'month_range/version'
 require_relative 'month_range/m_range'
 require_relative 'month_range/collection'
 require_relative 'month_range/error'
+require_relative 'month_range/month'
+require_relative 'month_range/service'
 module MonthRange
 end
