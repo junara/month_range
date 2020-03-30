7a5202cfd0ec2ce5b0b3ea7b768db101603f4167
junara <jun5araki@gmail.com>
2020-03-31 00:14:01 +0900

    Added yard.

--- lib/month_range/service.rb
+++ lib/month_range/service.rb
@@ -1,14 +1,35 @@
 # frozen_string_literal: true
 
 class MonthRange::Service
-  # @param
-  # [Date, Date]
-  # [start_month, end_month]
-  # @param
-  # [[Date, Date], [Date, Date],]
-  # [[start_month, end_month], [start_month, end_month],]
-  # @return
-  # [[Date, Date], [Date, Date],]
+  # Subtract range
+  # @param [MonthRange::MRange] range_array
+  # @param [Array] from_range_arrays
+  # @example from_range_arrays
+  #  from_range_arrays = [
+  #    [Date.parse('2020-01-01'), Date.parse('2020-04-01')],
+  #    [Date.parse('2020-07-01'), Date.parse('2020-10-01')],
+  #    [Date.parse('2020-12-01'), nil]
+  #  ]
+  # @example Simple
+  #  range_array = [Date.parse('2020-02-01'), Date.parse('2020-03-01')]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>], [#<Date: 2020-04-01 ((2458941j,0s,0n),+0s,2299161j)>, #<Date: 2020-04-01 ((2458941j,0s,0n),+0s,2299161j)>], [#<Date: 2020-07-01 ((2459032j,0s,0n),+0s,2299161j)>, #<Date: 2020-10-01 ((2459124j,0s,0n),+0s,2299161j)>], [#<Date: 2020-12-01 ((2459185j,0s,0n),+0s,2299161j)>, nil]]
+  # @example 複数期間にまたがる
+  #  range_array = [Date.parse('2020-03-01'), Date.parse('2020-08-01')]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-02-01 ((2458881j,0s,0n),+0s,2299161j)>], [#<Date: 2020-09-01 ((2459094j,0s,0n),+0s,2299161j)>, #<Date: 2020-10-01 ((2459124j,0s,0n),+0s,2299161j)>], [#<Date: 2020-12-01 ((2459185j,0s,0n),+0s,2299161j)>, nil]]
+  # @example 期間内
+  #  range_array = [Date.parse('2020-02-01'), Date.parse('2020-03-01')]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>], [#<Date: 2020-04-01 ((2458941j,0s,0n),+0s,2299161j)>, #<Date: 2020-04-01 ((2458941j,0s,0n),+0s,2299161j)>], [#<Date: 2020-07-01 ((2459032j,0s,0n),+0s,2299161j)>, #<Date: 2020-10-01 ((2459124j,0s,0n),+0s,2299161j)>], [#<Date: 2020-12-01 ((2459185j,0s,0n),+0s,2299161j)>, nil]]
+  # @example 終端なし
+  #  range_array = [Date.parse('2020-05-01'), nil]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-04-01 ((2458941j,0s,0n),+0s,2299161j)>]]
   def self.subtraction(range_array, from_range_arrays)
     collection = range_arrays_to_collection(from_range_arrays)
     start_month = range_array[0]
@@ -18,14 +39,36 @@ class MonthRange::Service
     collection.to_a
   end
 
-  # @param
-  # [Date, Date]
-  # [start_month, end_month]
-  # @param
-  # [[Date, Date], [Date, Date],]
-  # [[start_month, end_month], [start_month, end_month],]
-  # @return
-  # [[Date, Date], [Date, Date],]
+  # Union range
+  # @param [MonthRange::MRange] range_array
+  # @param [Array of MonthRange::MRange] from_range_arrays
+  # @return [Array of MonthRange::MRange]
+  # @example from_range_arrays
+  #  from_range_arrays = [
+  #    [Date.parse('2020-01-01'), Date.parse('2020-04-01')],
+  #    [Date.parse('2020-07-01'), Date.parse('2020-10-01')],
+  #    [Date.parse('2020-12-01'), nil]
+  #  ]
+  # @example Simple
+  #  range_array = [Date.parse('2020-03-01'), Date.parse('2020-05-01')]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-05-01 ((2458971j,0s,0n),+0s,2299161j)>], [#<Date: 2020-07-01 ((2459032j,0s,0n),+0s,2299161j)>, #<Date: 2020-10-01 ((2459124j,0s,0n),+0s,2299161j)>], [#<Date: 2020-12-01 ((2459185j,0s,0n),+0s,2299161j)>, nil]]
+  # @example 複数期間にまたがる
+  #  range_array = [Date.parse('2020-03-01'), Date.parse('2020-08-01')]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-10-01 ((2459124j,0s,0n),+0s,2299161j)>], [#<Date: 2020-12-01 ((2459185j,0s,0n),+0s,2299161j)>, nil]]
+  # @example 隣り合った期間を連結
+  #  range_array = [Date.parse('2020-05-01'), Date.parse('2020-06-01')]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, #<Date: 2020-10-01 ((2459124j,0s,0n),+0s,2299161j)>], [#<Date: 2020-12-01 ((2459185j,0s,0n),+0s,2299161j)>, nil]]
+  # @example 終端なし
+  #  range_array = [Date.parse('2020-05-01'), nil]
+  #  MonthRange::Service.add(range_array, from_range_arrays)
+  #
+  #  #=> [[#<Date: 2020-01-01 ((2458850j,0s,0n),+0s,2299161j)>, nil]]
   def self.add(range_array, from_range_arrays)
     collection = range_arrays_to_collection(from_range_arrays)
     start_month = range_array[0]
@@ -35,8 +78,9 @@ class MonthRange::Service
     collection.to_a
   end
 
-  # @param
-  # [[Date, Date], [Date, Date],]
+  # Change to MonthRange::Collection
+  # @param [[[Date, Date], [Date, Date],]] range_arrays
+  # @return [MonthRange::Collection]
   def self.range_arrays_to_collection(range_arrays)
     collection = MonthRange::Collection.new
     range_arrays.each do |range|
--- month_range.gemspec
+++ month_range.gemspec
@@ -4,19 +4,19 @@ require_relative 'lib/month_range/version'
 Gem::Specification.new do |spec|
   spec.name          = 'month_range'
   spec.version       = MonthRange::Version::VERSION
-  spec.authors       = ['junara']
+  spec.authors       = ['jungo araki']
   spec.email         = ['jun5araki@gmail.com']
 
   spec.summary       = 'Addition and subtraction for month range.'
   spec.homepage      = 'https://github.com/junara/month_range'
   spec.license       = 'MIT'
-  spec.required_ruby_version = Gem::Requirement.new('>= 2.3.0')
+  spec.required_ruby_version = Gem::Requirement.new('>= 2.6.0')
 
-  spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
 
   spec.metadata['homepage_uri'] = spec.homepage
   spec.metadata['source_code_uri'] = spec.homepage
   spec.metadata['changelog_uri'] = spec.homepage
+  spec.metadata['documentation_uri'] = 'https://rubydoc.info/gems/month_range'
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
